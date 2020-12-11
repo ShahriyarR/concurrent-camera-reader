40349ff4379c8c0ea3644882dbde3b04a4a98736
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2020-12-12 03:18:26 +0400

    Code changes for reading and showing frames from 2 cameras without async

--- async_frame_reader/video_async.py
+++ async_frame_reader/video_async.py
@@ -1,10 +1,32 @@
-def read_frame(capture):
-    # The method for using OpenCV grab() - retrieve()
-    # We are not using read() here because, documentation insists that it is slower in multi-thread environment.
-    capture.grab()
-    ret, frame = capture.retrieve()
-    print(frame)
-    if not ret:
-        print("empty frame")
-        return
-    return frame
+from typing import Dict
+import cv2 as cv
+import numpy as np
+
+
+class MultiCameraCapture:
+
+    def __init__(self, sources: Dict) -> None:
+        assert sources
+        print(sources)
+        self.captures = {}
+        for camera_name, link in sources.items():
+            cap = cv.VideoCapture(link)
+            print(camera_name)
+            assert cap.isOpened()
+            self.captures[camera_name] = cap
+
+    @staticmethod
+    def read_frame(capture):
+        # The method for using OpenCV grab() - retrieve()
+        # We are not using read() here because, documentation insists that it is slower in multi-thread environment.
+        # capture.grab()
+        # ret, frame = capture.retrieve()
+        ret, frame = capture.read()
+        if not ret:
+            print("empty frame")
+            return
+        return frame
+
+    @staticmethod
+    def show_frame(window_name: str, frame: np.array):
+        cv.imshow(window_name, frame)
