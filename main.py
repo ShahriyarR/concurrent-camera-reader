10d51c68e8bba6d5da9cee80d94cf6f8e95ad72d
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2020-12-12 02:37:30 +0400

    Adding current time to the video frame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import datetime
 from async_frame_reader.video_async import read_frame
 
 if __name__ == "__main__":
@@ -7,6 +8,21 @@ if __name__ == "__main__":
     print(cap)
     while True:
         frame = read_frame(cap)
+
+        font = cv.FONT_HERSHEY_SCRIPT_COMPLEX
+
+        # Get date and time and
+        # save it inside a variable
+        dt = str(datetime.datetime.now())
+
+        # put the dt variable over the
+        # video frame
+        frame = cv.putText(frame, dt,
+                           (10, 100),
+                           font, 1,
+                           (210, 155, 155),
+                           4, cv.LINE_8)
+
         cv.imshow("Frontal Camera", frame)
         if cv.waitKey(1) == 27:
             break
