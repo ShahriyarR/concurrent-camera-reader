afde85d7fd1efdb5c36a23c6d140ae2fc70b1f82
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2020-12-13 16:21:37 +0400

    Code changes for adding extra async support - it will clarify the problem itself more

--- main.py
+++ main.py
@@ -6,16 +6,28 @@ from utils.face_detection import run_face_detection
 from utils.add_datetime import add_timestamp_to_frame
 
 
+async def run_fd_time(frame):
+    # Show 2nd attempt then
+    task1 = asyncio.create_task(add_timestamp_to_frame(frame))
+    task2 = asyncio.create_task(run_face_detection(frame))
+    await asyncio.gather(task1, task2)
+    await asyncio.sleep(0.01)
+
+
 async def main(captured_obj):
     while True:
         async for camera_name, cap in captured.async_camera_gen():
             frame = await captured_obj.read_frame(cap)
-            frame = await add_timestamp_to_frame(frame)
-            frame = run_face_detection(frame)
 
+            # Show 1st attempt first
+            # task1 = asyncio.create_task(add_timestamp_to_frame(frame))
+            # task2 = asyncio.create_task(run_face_detection(frame))
+            # await asyncio.gather(task1, task2)
+            await run_fd_time(frame)
             await captured_obj.show_frame(camera_name, frame)
             if cv.waitKey(1) == 27:
                 break
+            await  asyncio.sleep(0.01)
 
 
 if __name__ == "__main__":
